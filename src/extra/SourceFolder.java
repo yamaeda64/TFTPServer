c75111736207d1c7680e605ab981f204511173cf
yamaeda64 <joakim_bergqvist@hotmail.com>
2018-02-27 12:26:12 +0100

    Added an extra class that we shouldn't update on GIT to keep personal SourceFolder directory

---
+++ src/extra/SourceFolder.java
@@ -0,0 +1,16 @@
+package extra;
+
+/**
+ * Created by joakimbergqvist on 2018-02-27.
+ */
+public class SourceFolder
+{
+    public static String getWriteFolder()
+    {
+        return "/Users/joakimbergqvist/Documents/Joakims/Document/Network/Assignment3/SourceFolder/Write/";
+    }
+    public static String getReadFolder()
+    {
+        return "/Users/joakimbergqvist/Documents/Joakims/Document/Network/Assignment3/SourceFolder/Read/";
+    }
+}
